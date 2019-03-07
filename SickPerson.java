169ed695fbcea0b20c6e11ea2b3838e394dc461e
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-03-07 15:31:21 -0600

    Update SickPerson.java

--- SickPerson.java
+++ SickPerson.java
@@ -1,12 +1,24 @@
 public class SickPerson extends Person {
 
+	/**
+	 * severity of the patient's illness depicted as an int (higher number, more severe).
+	 */
 	private int severity;
 	
+	/**
+	 * constructor for a SickPerson
+	 * @param name, name of patient
+	 * @param age, patient's age
+	 * @param severity, severity of the patients illness (as an int)
+	 */
 	public SickPerson(String name, int age, int severity) {
 		super(name, age);
 		this.severity = severity;
 	}
 	
+	/**
+	 * follows the structure of the compareTo() method. returns 0 automatically if the person 'p' is not a SickPerson.
+	 */
 	@Override
 	protected int compareToImpl(Person p) {
 		if (p instanceof SickPerson) {
@@ -25,6 +37,10 @@ public class SickPerson extends Person {
 		}
 	}
 	
+	/**
+	 * returns a string comprised of the superclass' toString() method and the String "Severity level (severity)".
+	 * Follows the format "(superclass' toString()) Severity level (severity)".
+	 */
 	@Override
 	public String toString() {
 		return String.format("%s Severity level %d", super.toString(), severity);
