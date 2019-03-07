7b8582ddabb1eb3a78dc8cbfcdcc69eac3a7ea70
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-03-07 12:23:09 -0600

    Create Todo.java

---
+++ Todo.java
@@ -0,0 +1,37 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-07
+OU ID_______________: 113472850
+OU EMAIL____________: aidenwitt@ou.edu
+
+Tasks:
+1. Complete the methods outlined for "HealthyPerson.java"
+2. Complete the methods outlined for "SickPerson.java"
+3. Complete the methods outlined for "StackHospital.java"
+4. Complete the methods outlined for "QueueHospital.java"
+5. Complete the methods outlined for "PriorityQueueHospital.java"
+6. Test for coverage
+7. Try and make webcat do its job
+8. write a todo last second
+
+Estimated Time (minutes):
+1. 10
+2. 5
+3. 20
+4. 10
+5. 10
+6. 25
+7. 10
+8. 1
+
+Actual Time (minutes):
+1. 10
+2. 2
+3. 15
+4. 5
+5. 5
+6. 10
+7. 0 (gave up, couldnt upload first time)
+8. 2
+
+*/}
