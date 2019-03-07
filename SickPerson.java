d39999c0a78a59712d48c0b2bc8bfb05dd9667d9
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-03-07 12:11:55 -0600

    Add files via upload

---
+++ SickPerson.java
@@ -0,0 +1,33 @@
+public class SickPerson extends Person {
+
+	private int severity;
+	
+	public SickPerson(String name, int age, int severity) {
+		super(name, age);
+		this.severity = severity;
+	}
+	
+	@Override
+	protected int compareToImpl(Person p) {
+		if (p instanceof SickPerson) {
+			if (((SickPerson) p).severity > (this.severity)) {
+				return 1;
+			}
+			else if (((SickPerson) p).severity < (this.severity)){
+				return -1;
+			}
+			else {
+				return 0;
+			}
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s Severity level %d", super.toString(), severity);
+	}
+	
+}
