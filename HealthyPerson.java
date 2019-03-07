e8a5ddf44bfc6e59a16bb27b5034b0e58b7471f1
aidenwitt <47369128+aidenwitt@users.noreply.github.com>
2019-03-07 15:30:13 -0600

    Update HealthyPerson.java

--- HealthyPerson.java
+++ HealthyPerson.java
@@ -1,31 +1,39 @@
 
 public class HealthyPerson extends Person {
 
+	/**
+	 * The patients reason for being admitted to the hospital (String)
+	 */
 	private String reason;
 	
+	/**
+	 * Constructor for a healthy person 
+	 * @param name, name of the patient
+	 * @param age, patients age
+	 * @param reason, reason for admittance
+	 */
 	public HealthyPerson(String name, int age, String reason) {
 		super(name, age);
 		this.reason = reason;
 	}
 
+	/** 
+	 * Method for sorting Healthy patients, mainly uses compareTo, but also returns 0 when the Person 'p' is not a Healthy Person
+	 */
 	@Override
 	protected int compareToImpl(Person p) {
-		if (p instanceof SickPerson) {
-			if (((SickPerson) p).getName().compareTo(this.getName()) > 0) {
-				return 1;
-			}
-			else if (((SickPerson) p).getName().compareTo(this.getName()) < 0){
-				return -1;
-			}
-			else {
-				return 0;
-			}
+		if (p instanceof HealthyPerson) {
+			return ((HealthyPerson) p).getName().compareTo(this.getName());
 		}
 		else {
 			return 0;
 		}
 	}
 	
+	/**
+	 * returns a string comprised of the superclass' toString() and the String "In for (reason)".
+	 * returns String in format "(Superclass' toString()) In for (reason)".
+	 */
 	@Override
 	public String toString() {
 		return String.format("%s In for %s", super.toString(), reason);
